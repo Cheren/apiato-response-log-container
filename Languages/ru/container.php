050de8b3a102e153f5255a44759779d9aff2ea1f
Cheren <kalistratov.s.m@gmail.com>
2025-04-23 15:35:54 +0400

    Add DeleteResponseLogCommand

--- Languages/ru/container.php
+++ Languages/ru/container.php
@@ -1,18 +1,25 @@
 <?php
 
 /**
- * ERP system
+ * APIATO setting container.
  *
- * This file is part of the ERM system package.
+ * This file is part of the APIATO setting container.
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  *
- * @license     https://kalistratov.ru/licenses/erp Proprietary license
- * @copyright   Copyright (C) kalistratov.ru, All rights reserved ©.
- * @link        https://kalistratov.ru
- * @author      Sergey Kalistratov <sergey@kalistratov.ru>
+ * @license    Proprietary
+ * @copyright  Copyright (C) kalistratov.ru, All rights reserved.
+ * @link       https://kalistratov.ru
  */
 
 return [
-    'name' => 'Журнал ошибок API'
+    'name' => 'Журнал ошибок API',
+    'command' => [
+        'delete' => [
+            'description' => 'Удалить лог ошибок из базы данных',
+            'early_for' => 'Количество дней раннее от которого удалить логи ошибок',
+            'no_data_to_delete' => 'Нет данных для удаления.',
+            'result' => 'Всего удленно: :total'
+        ]
+    ]
 ];
